afef93d92210491b7d49e8e0c06de603b1581b28
Divyanshupandey007 <pandey.divyanshu03@gmail.com>
2025-04-23 12:01:02 +0000

    commit

--- springapp/src/main/java/com/examly/springapp/model/Feedback.java
+++ springapp/src/main/java/com/examly/springapp/model/Feedback.java
@@ -3,9 +3,11 @@ package com.examly.springapp.model;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Feedback {
+    @Id
     private Long feedbackId;
     private String feedbackText;
     private LocalDate date;
