8bdf7461cf3611b80985b618f200f0b723b925dc
harshini-178 <harshinimamidala2003@gmail.com>
2025-04-24 06:59:54 +0000

    updated

--- springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
@@ -2,10 +2,12 @@ package com.examly.springapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.examly.springapp.model.User;
 import com.examly.springapp.repository.UserRepo;
 
+@Service
 public class UserServiceImpl implements UserService{
 
     @Autowired
