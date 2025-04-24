9ac84d8185ebe1d413ce7d586202b42ecd027e64
isajalsingh <sajalkr2011@gmail.com>
2025-04-24 06:54:40 +0000

    created Loan Controller and Service

---
+++ springapp/src/main/java/com/examly/springapp/controller/LoanController.java
@@ -0,0 +1,67 @@
+package com.examly.springapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examly.springapp.model.Loan;
+import com.examly.springapp.service.LoanServiceImpl;
+
+@RestController
+@RequestMapping("/api/loan")
+public class LoanController {
+
+    @Autowired
+    LoanServiceImpl loanService;
+
+    @PostMapping
+    public ResponseEntity<Loan> addLoan(@RequestBody Loan loan) {
+        Loan createdLoan = loanService.addLoan(loan);
+        if(createdLoan != null)
+            return ResponseEntity.status(201).body(createdLoan);
+        return ResponseEntity.status(400).body(null);
+    }
+
+    @GetMapping("/{loanId}")
+    public ResponseEntity<Loan> viewLoanById(@PathVariable Long loanId) {
+        Loan loan = loanService.getLoanById(loanId);
+        if(loan != null)
+            return ResponseEntity.status(200).body(loan);
+        return ResponseEntity.status(404).body(null);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Loan>> viewAllLoans() {
+        List<Loan> loans = loanService.getAllLoans();
+        if(!loans.isEmpty())
+            return ResponseEntity.status(200).body(loans);
+        return ResponseEntity.status(404).body(null);
+    }
+
+    @PutMapping("/{loanId}")
+    public ResponseEntity<Loan> editLoan(@PathVariable Long loanId, @RequestBody Loan loanDetails) {
+        Loan updatedLoan = loanService.updateLoan(loanId, loanDetails);
+        if(updatedLoan != null)
+            return ResponseEntity.status(200).body(updatedLoan);
+        return ResponseEntity.status(404).body(null);
+    }
+
+    @DeleteMapping("/{loanId}")
+    public ResponseEntity<Loan> deleteLoan(@PathVariable Long loanId) {
+        Loan deletedLoan = loanService.deleteLoan(loanId);
+        if(deletedLoan != null)
+            return ResponseEntity.status(200).body(deletedLoan);
+        return ResponseEntity.status(404).body(null);
+    }
+
+}
---
+++ springapp/src/main/java/com/examly/springapp/service/LoanServiceImpl.java
@@ -0,0 +1,54 @@
+package com.examly.springapp.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.examly.springapp.model.Loan;
+
+@Service
+public class LoanServiceImpl implements LoanService {
+
+    @Autowired
+    private LoanRepository loanRepository;
+
+    @Override
+    public Loan addLoan(Loan loan) {
+        return loanRepository.save(loan);
+    }
+
+    @Override
+    public Loan getLoanById(Long loanId) {
+        Optional<Loan> loan = loanRepository.findById(loanId);
+        return loan.orElse(null);
+
+    }
+
+    @Override
+    public List<Loan> getAllLoans() {
+        return loanRepository.findAll();
+    }
+
+    @Override
+    public Loan updateLoan(Long loanId, Loan updatedLoan) {
+        if (loanRepository.existsById(loanId)) {
+            updatedLoan.setLoanId(loanId);
+            return loanRepository.save(updatedLoan);
+        }
+        return null;
+    }
+
+    @Override
+    public Loan deleteLoan(Long loanId) {
+
+        Optional<Loan> loan = loanRepository.findById(loanId);
+        if (loan.isPresent()) {
+            loanRepository.deleteById(loanId);
+            return loan.get();
+        }
+        return null;
+    }
+
+}
