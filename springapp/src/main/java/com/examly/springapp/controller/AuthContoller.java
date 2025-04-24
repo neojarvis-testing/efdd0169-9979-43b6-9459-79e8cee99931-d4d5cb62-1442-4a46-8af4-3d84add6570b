7d5b66d450f1982fbae4066c3ce147cfc9ceb881
harshini-178 <harshinimamidala2003@gmail.com>
2025-04-24 06:33:09 +0000

    updated user

--- springapp/src/main/java/com/examly/springapp/controller/AuthContoller.java
+++ springapp/src/main/java/com/examly/springapp/controller/AuthContoller.java
@@ -4,24 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.examly.springapp.model.User;
 import com.examly.springapp.service.UserServiceImpl;
 
 @RestController
+@RequestMapping("/api")
 public class AuthContoller {
 
     @Autowired
     UserServiceImpl service;
 
-    @PostMapping("/api/register")
+    @PostMapping("/register")
     public ResponseEntity<?> createUser(@RequestBody User user){
         user=service.createUser(user);
         return ResponseEntity.status(201).body(user);
     }
 
-    @PostMapping("/api/login")
+    @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody User user){
         user=service.loginUser(user);
         return ResponseEntity.status(201).body(user);
