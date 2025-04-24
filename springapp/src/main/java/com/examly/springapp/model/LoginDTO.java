7989c2ed81a6fdd57b997e4e9511ccacf39a9547
Divyanshupandey007 <pandey.divyanshu03@gmail.com>
2025-04-24 06:11:35 +0000

    added loginDTO and feedBackDTO with validation with minor changes

---
+++ springapp/src/main/java/com/examly/springapp/model/LoginDTO.java
@@ -0,0 +1,37 @@
+package com.examly.springapp.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class LoginDTO {
+	
+	@Email(message="Email should be valid")
+	@NotBlank(message="Email is mandatory")
+    private String email;
+	
+	@NotBlank(message="Password is mandatory")
+	@Size(min=8,message="Password should be at least 8 characters long")
+    private String password;
+	
+    private String userRole;
+    
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+}
