7116b1599f701a80271b299e50f7381041c104d5
Divyanshupandey007 <pandey.divyanshu03@gmail.com>
2025-04-24 07:30:21 +0000

    Errors resolved

--- springapp/src/main/java/com/examly/springapp/repository/LoanApplicationRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/LoanApplicationRepo.java
@@ -1,9 +1,11 @@
 package com.examly.springapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.examly.springapp.model.LoanApplication;
 
+@Repository
 public interface LoanApplicationRepo extends JpaRepository<LoanApplication,Long>{
     
 }
