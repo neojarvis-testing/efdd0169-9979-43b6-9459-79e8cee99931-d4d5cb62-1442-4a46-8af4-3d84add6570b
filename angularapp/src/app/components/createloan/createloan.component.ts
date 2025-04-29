6df2bb116e2c52d655c4e299545cfb41c90ad201
nitishgupta123 <nitishgupta9565592927@gmail.com>
2025-04-29 04:55:12 +0000

    created components and updated ts

---
+++ angularapp/src/app/components/createloan/createloan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-createloan',
+  templateUrl: './createloan.component.html',
+  styleUrls: ['./createloan.component.css']
+})
+export class CreateloanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angularapp/src/app/components/loanform/loanform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loanform',
+  templateUrl: './loanform.component.html',
+  styleUrls: ['./loanform.component.css']
+})
+export class LoanformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
