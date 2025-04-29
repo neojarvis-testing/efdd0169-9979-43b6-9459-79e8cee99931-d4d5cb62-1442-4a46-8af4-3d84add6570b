508069747701dd442fe5e2cc128f31b06ca6fd31
isajalsingh <sajalkr2011@gmail.com>
2025-04-29 08:48:45 +0000

    error resolved

--- angularapp/src/app/services/loan.service.ts
+++ angularapp/src/app/services/loan.service.ts
@@ -4,110 +4,68 @@ import { Loan } from '../models/loan.model';
 import { LoanApplication } from '../models/loanapplication.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class LoanService {
-
+ 
   baseUrl:string=''
-
-
+ 
+ 
   constructor(private http:HttpClient) {
     this.baseUrl=ApiUrl.apiUrl
     console.log('url:'+this.baseUrl)
   }
-
+ 
   getAllLoans(): Observable<any> {
     return this.http.get(`${this.baseUrl}/loan`);
   }
-
+ 
   deleteLoan(loanId: number): Observable<any> {
     return this.http.delete(`${this.baseUrl}/loan/${loanId}`);
   }
-
+ 
   getLoanById(id: number): Observable<any> {
     return this.http.get(`${this.baseUrl}/loan/${id}`);
   }
-
+ 
   addLoan(requestObject: Loan): Observable<any> {
     return this.http.post(`${this.baseUrl}/loan`, requestObject);
   }
-
+ 
   updateLoan(id: number, requestObject: Loan): Observable<any> {
     return this.http.put(`${this.baseUrl}/loan/${id}`, requestObject);
   }
-
+ 
   getAppliedLoans(userId: number): Observable<any> {
     return this.http.get(`${this.baseUrl}/loanapplication/user/${userId}`);
   }
-
+ 
   deleteLoanApplication(loanId: number): Observable<any> {
     return this.http.delete(`${this.baseUrl}/loanapplication/${loanId}`);
   }
-
+ 
   addLoanApplication(formData: FormData): Observable<any> {
     return this.http.post(`${this.baseUrl}/loanapplication`, formData);
   }
-
+ 
   getAllLoanApplications(): Observable<any> {
     return this.http.get(`${this.baseUrl}/loanapplication`);
   }
-
+ 
   updateLoanStatus(id: number, loanApplication: LoanApplication): Observable<any> {
     return this.http.put(`${this.baseUrl}/loanapplication/${id}`, loanApplication);
-
-    return this.http.delete(`${this.baseUrl}loan/${loanId}`);
   }
-
-  getLoanById(id: number): Observable<any> {
-    return this.http.get(`${this.baseUrl}loan/${id}`);
-  }
-
-  addLoan(requestObject: Loan): Observable<any> {
-    return this.http.post(`${this.baseUrl}loan`, requestObject);
-  }
-
-  updateLoan(id: number, requestObject: Loan): Observable<any> {
-    return this.http.put(`${this.baseUrl}loan/${id}`, requestObject);
-  }
-
-  getAppliedLoans(userId: number): Observable<any> {
-    return this.http.get(`${this.baseUrl}loanapplication/user/${userId}`);
-  }
-
-  deleteLoanApplication(loanId: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}loanapplication/${loanId}`);
-  }
-
-  addLoanApplication(formData: FormData): Observable<any> {
-    return this.http.post(`${this.baseUrl}loanapplication`, formData);
-  }
-
-  getAllLoanApplications(): Observable<any> {
-    return this.http.get(`${this.baseUrl}loanapplication`);
-  }
-
-  updateLoanStatus(id: number, loanApplication: LoanApplication): Observable<any> {
-    return this.http.put(`${this.baseUrl}loanapplication/${id}`, loanApplication);
-
-  }
-
+ 
   // New methods to fetch loan status and monthly loan data
   getLoanStatus(): Observable<any> {
-
     return this.http.get(`${this.baseUrl}/loan/status`);
   }
-
+ 
   getMonthlyLoanData(): Observable<any> {
     return this.http.get(`${this.baseUrl}/loan/monthly`);
-
-    return this.http.get(`${this.baseUrl}loan/status`);
   }
-
-  getMonthlyLoanData(): Observable<any> {
-    return this.http.get(`${this.baseUrl}loan/monthly`);
-
-  }
-
+ 
 }
+ 
