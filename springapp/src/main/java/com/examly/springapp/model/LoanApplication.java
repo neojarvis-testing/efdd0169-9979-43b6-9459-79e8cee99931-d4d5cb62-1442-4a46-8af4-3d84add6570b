cb28159bca67e110bd00e85fbd4259d10e4c2b05
Divyanshupandey007 <pandey.divyanshu03@gmail.com>
2025-04-30 06:29:47 +0000

    Implemented Lombok

--- springapp/src/main/java/com/examly/springapp/model/LoanApplication.java
+++ springapp/src/main/java/com/examly/springapp/model/LoanApplication.java
@@ -10,8 +10,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoanApplication {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,85 +36,4 @@ public class LoanApplication {
     @ManyToOne
     @JoinColumn(name="loanId")
     private Loan loan;
-    public Long getLoanApplicationId() {
-        return loanApplicationId;
-    }
-    public void setLoanApplicationId(Long loanApplicationId) {
-        this.loanApplicationId = loanApplicationId;
-    }
-    public LocalDate getSubmissionDate() {
-        return submissionDate;
-    }
-    public void setSubmissionDate(LocalDate submissionDate) {
-        this.submissionDate = submissionDate;
-    }
-    public String getLoanStatus() {
-        return loanStatus;
-    }
-    public void setLoanStatus(String loanStatus) {
-        this.loanStatus = loanStatus;
-    }
-    public String getFarmLocation() {
-        return farmLocation;
-    }
-    public void setFarmLocation(String farmLocation) {
-        this.farmLocation = farmLocation;
-    }
-    public String getFarmerAddress() {
-        return farmerAddress;
-    }
-    public void setFarmerAddress(String farmerAddress) {
-        this.farmerAddress = farmerAddress;
-    }
-    public double getFarmSizeInAcres() {
-        return farmSizeInAcres;
-    }
-    public void setFarmSizeInAcres(double farmSizeInAcres) {
-        this.farmSizeInAcres = farmSizeInAcres;
-    }
-    public String getFarmpurpose() {
-        return farmpurpose;
-    }
-    public void setFarmpurpose(String farmpurpose) {
-        this.farmpurpose = farmpurpose;
-    }
-    public String getFile() {
-        return file;
-    }
-    public void setFile(String file) {
-        this.file = file;
-    }
-    public User getUser() {
-        return user;
-    }
-    public void setUser(User user) {
-        this.user = user;
-    }
-    public Loan getLoan() {
-        return loan;
-    }
-    public void setLoan(Loan loan) {
-        this.loan = loan;
-    }
-    //Constructors
-    public LoanApplication() {
-    }
-    public LoanApplication(Long loanApplicationId, LocalDate submissionDate, String loanStatus, String farmLocation,
-            String farmerAddress, double farmSizeInAcres, String farmpurpose, String file, User user, Loan loan) {
-        this.loanApplicationId = loanApplicationId;
-        this.submissionDate = submissionDate;
-        this.loanStatus = loanStatus;
-        this.farmLocation = farmLocation;
-        this.farmerAddress = farmerAddress;
-        this.farmSizeInAcres = farmSizeInAcres;
-        this.farmpurpose = farmpurpose;
-        this.file = file;
-        this.user = user;
-        this.loan = loan;
-    }
-    
-    
-    
-    
-
 }
--- springapp/src/main/java/com/examly/springapp/model/LoanDTO.java
+++ springapp/src/main/java/com/examly/springapp/model/LoanDTO.java
@@ -2,7 +2,13 @@ package com.examly.springapp.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoanDTO {
 
     @NotBlank(message ="Loan must not be empty")
@@ -17,48 +23,4 @@ public class LoanDTO {
     private int repaymentTenure;
     private String eligibility;
     private String documentsRequired;
-    public String getLoanType() {
-        return loanType;
-    }
-    public void setLoanType(String loanType) {
-        this.loanType = loanType;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public double getInterestRate() {
-        return interestRate;
-    }
-    public void setInterestRate(double interestRate) {
-        this.interestRate = interestRate;
-    }
-    public double getMaximumAmount() {
-        return maximumAmount;
-    }
-    public void setMaximumAmount(double maximumAmount) {
-        this.maximumAmount = maximumAmount;
-    }
-    public int getRepaymentTenure() {
-        return repaymentTenure;
-    }
-    public void setRepaymentTenure(int repaymentTenure) {
-        this.repaymentTenure = repaymentTenure;
-    }
-    public String getEligibility() {
-        return eligibility;
-    }
-    public void setEligibility(String eligibility) {
-        this.eligibility = eligibility;
-    }
-    public String getDocumentsRequired() {
-        return documentsRequired;
-    }
-    public void setDocumentsRequired(String documentsRequired) {
-        this.documentsRequired = documentsRequired;
-    }
-    
-    
 }
